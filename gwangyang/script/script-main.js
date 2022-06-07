bc4b46e4fbd7761a3a294d27e52bc67b2cfa90cd
ynu-pw <106992598+ynu-pw@users.noreply.github.com>
2022-06-07 15:11:29 +0900

    Update script-main.js

--- gwangyang/script/script-main.js
+++ gwangyang/script/script-main.js
@@ -8,10 +8,6 @@ $(function(){
     // var $subs = $(".sub");
     // var $sub1_menu = $("#sub1_menu");
     // var $sub1 = $("#sub1");
-
-    $nav_menu_a.on("click", function(event) {
-        event.preventDefault();
-    });
     
      /* $nav_menu.on("click", function() {
          if($(this).find(".sub").hasClass("active")) {
